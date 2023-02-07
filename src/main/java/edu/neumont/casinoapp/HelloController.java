45f4a958f341ea78624a0c43d9c2efbf72064fcf
0xJest <EndlessJest@gmail.com>
2023-02-07 14:17:48 -0700

    Added future information for scene transitions

--- src/main/java/edu/neumont/casinoapp/HelloController.java
+++ src/main/java/edu/neumont/casinoapp/HelloController.java
@@ -11,4 +11,13 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    // Scene Transitions:
+    // Will need to be setup in individual controllers
+
+    // Stage stage = (Stage) label.getScene().getWindow();
+    // FXMLLoader sceneLoader = new FXMLLoader(getClass().getResource("yourFile.fxml"));
+    // Parent newScene = sceneLoader.load();
+    // stage.setScene(new Scene(newScene));
+
 }
